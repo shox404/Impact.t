db94544c109129792a98e4ce49189c729fb44f09
Shoxruh <shxxx404@gmail.com>
2024-08-17 23:20:03 +0500

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import Navbar from "./components/navbar";
 import Router from "./router";
 
 export default function App() {
   return (
     <main>
+      <Navbar />
       <Router />
     </main>
   );
--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,5 +1,22 @@
 import "./styles.scss";
+import logo from "../../assets/materials/logo.png";
 
 export default function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <nav>
+      <ul>
+        <li>GALLERY</li>
+        <li>OFFICE</li>
+        <li>OPEN SPACE</li>
+        <li>MEETING ROOMS</li>
+      </ul>
+      <img src={logo} />
+      <ul>
+        <li>GALLERY</li>
+        <li>OFFICE</li>
+        <li>OPEN SPACE</li>
+        <li>MEETING ROOMS</li>
+      </ul>
+    </nav>
+  );
 }
