65a1470d3ab90a5f19e92a2037f5e100b5ee6966
[Musab Kamaldinov] <you@example.com>
2024-08-17 22:29:54 +0500

    musb

---
+++ src/pages/home/musAb.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MusAb = () => {
+    return (
+        <div>
+              <h1 className="text-black text-[40px]">Zetfri</h1>
+        </div>
+    );
+}
+
+export default MusAb;
