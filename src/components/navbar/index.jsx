aee78699cea78a2ff34aa553813b338b892ab497
Shoxruh <shxxx404@gmail.com>
2024-08-17 18:49:56 +0500

    Router dom

---
+++ src/components/navbar/index.jsx
@@ -0,0 +1,5 @@
+import "./styles.scss";
+
+export default function Navbar() {
+  return <div>Navbar</div>;
+}
---
+++ src/pages/home/index.jsx
@@ -0,0 +1,5 @@
+import "./styles.scss";
+
+export default function Home() {
+  return <div>Home</div>;
+}
