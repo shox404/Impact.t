df409d1a61d1689d5286065e1d70b9aff4053a3a
IbragimovaG <ibragimoffa.gul0706@gmail.com>
2024-08-18 15:03:57 +0500

    gulnoza

--- src/router.jsx
+++ src/router.jsx
@@ -1,10 +1,14 @@
-import { Routes, Route } from "react-router-dom";
-import AboutStudio from './pages/AboutStudio/AboutStudio'
-export default function Router() {
-  return (
-    <Routes>
-      <Route path="/" element={null} />
-      <Route path="/" element={<AboutStudio/>}/>
-    </Routes>
-  );
+import { Routes, Route } from "react-router-dom"; 
+import Index from "./pages/open-space"; 
+import Home from "./pages/home"; 
+import AboutStudio from "./pages/AboutStudio"; 
+ 
+export default function Router() { 
+  return ( 
+    <Routes> 
+      <Route path="/" element={<Home />} /> 
+      <Route path="/open-space" element={<Index />} /> 
+      <Route path="/about-studio" element={<AboutStudio />} /> 
+    </Routes> 
+  ); 
 }
