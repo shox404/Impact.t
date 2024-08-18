aaf4d5b28b459eb3eed152a5e9ddfa2d025b2b7f
Shoxruh <shxxx404@gmail.com>
2024-08-18 20:12:56 +0500

    Navbar done

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,16 +1,34 @@
 import logoWhite from "../../assets/materials/logo-white.png";
 import logoBlack from "../../assets/materials/logo-black.png";
 import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
 import "./styles.scss";
 
 export default function Navbar({ color }) {
+  const [state, setState] = useState(false);
+  const [top, setTop] = useState(0);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      setTop(window.scrollY);
+    });
+    if (window.innerWidth >= 768) setState(true);
+    window.addEventListener("resize", () => {
+      if (window.innerWidth >= 768) setState(true);
+    });
+  }, []);
+
+  const toggle = () => setState(!state);
+
   return (
     <nav
-      className={`w-full absolute top-0 grid grid-cols-3 px-14 py-8 align-baseline text-xs font-normal z-50 ${
-        color == "black" ? "text-black" : "text-white"
-      }`}
+      className={`w-full fixed top-0 grid grid-cols-3 px-14 py-8 align-baseline text-xs font-normal z-50 transition-all md:grid-cols-1 md:p-14 ${
+        top > 100 ? (color == "black" ? "bg-white" : "bg-black") : ""
+      } ${
+        color == "black" ? "text-black md:bg-white" : "text-white md:bg-black"
+      } ${state ? 0 : "top-[-100%]"}`}
     >
-      <ul className="flex gap-9">
+      <ul className="flex gap-9 md:flex-wrap md:justify-center">
         <li>GALLERY</li>
         <li>OFFICE</li>
         <Link to={"/open-space"}>
@@ -18,8 +36,11 @@ export default function Navbar({ color }) {
         </Link>
         <li className="text-nowrap">MEETING ROOMS</li>
       </ul>
-      <img src={color == "black" ? logoBlack : logoWhite} className="m-auto" />
-      <ul className="flex gap-9 justify-end align-baseline">
+      <img
+        src={color == "black" ? logoBlack : logoWhite}
+        className="m-auto md:my-10"
+      />
+      <ul className="flex gap-9 justify-end align-baseline md:flex-wrap md:justify-center">
         <li>NEWS</li>
         <li>CONTACTS</li>
         <select value={"EN"} className="bg-transparent outline-none">
@@ -32,7 +53,12 @@ export default function Navbar({ color }) {
         </select>
         <li className="text-nowrap">BOOKING SPACE</li>
       </ul>
-      {/* <button>menu</button> */}
+      <button
+        onClick={toggle}
+        className="hidden fixed top-5 right-5 py-2 px-4 rounded-full md:inline bg-black"
+      >
+        MENU
+      </button>
     </nav>
   );
 }
