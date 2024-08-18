0ba46678b335ec854c20e8da758189b63d181b36
IbragimovaG <ibragimoffa.gul0706@gmail.com>
2024-08-18 14:09:37 +0500

    Gulnoza

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
 import Router from "./router";
-import AboutStudio from './pages/AboutStudio/AboutStudio'
 export default function App() {
   return (
     <main>
       <Router />
-      <AboutStudio/>
     </main>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 module.exports = {
   content: ["./src/**/*.{html,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage:{
+        'backgroundImageAboutStudio':"url('/src/assets/AboutStudio10.png')",
+        'backgroundImageAboutStudio2':"url('/src/assets/AboutStudio11.png')",
+      }
+    },
   },
   plugins: [],
 }
