d560b342deb9cb014fba04384886e1bede4714fd
Shoxruh <shxxx404@gmail.com>
2024-08-18 09:50:29 +0500

    Update

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,24 +5,10 @@ module.exports = {
   ],
   theme: {
     screens: {
-<<<<<<< HEAD
       xl: { max: "1279px" },
       lg: { max: "1023px" },
       md: { max: "767px" },
       sm: { max: "639px" },
-=======
-      xl: {max: '1279px'},
-      // => @media (max-width: 1279px) { ... }
-
-      lg: {max: '1023px'},
-      // => @media (max-width: 1023px) { ... }
-
-      md: {max: '767px'},
-      // => @media (max-width: 767px) { ... }
-
-      sm: {max: '639px'},
-      // => @media (max-width: 639px) { ... }
->>>>>>> 18b50ff30cc6b92c9b37541b68f8fa9cd5261dc7
     },
     extend: {},
   },
