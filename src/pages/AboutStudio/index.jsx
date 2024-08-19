c8687f6f35351edba65251521508b09c0b2d4898
IbragimovaG <ibragimoffa.gul0706@gmail.com>
2024-08-19 16:03:35 +0500

    GulnozaMedia

--- src/pages/AboutStudio/index.jsx
+++ src/pages/AboutStudio/index.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import Navbar from './../../components/navbar/index';
-import Footer from '../../components/footer';
 
 export default function Index() {
     return (
