23b35c3fc8b42d3be7afa9fe9dd4a6951ec7f5ef
IbragimovaG <ibragimoffa.gul0706@gmail.com>
2024-08-19 16:01:19 +0500

    GulnozaMedia

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import Footer from "./components/footer";
 import Router from "./router";
-import AboutStudio from './pages/AboutStudio/index'
 export default function App() {
   return (
     <main>
       <Router />
-      <AboutStudio/>
       <Footer/>
     </main>
   );
