50ffa8fd89a0938262e97b082855253bc5236e47
Muhisn200 <uusa6736@gmail.com>
2024-08-19 16:21:28 +0500

    mushin

--- src/pages/open-space/index.jsx
+++ src/pages/open-space/index.jsx
@@ -12,7 +12,7 @@ function Index(props) {
             <div
                 className="w-full h-screen bg-black   sm:justify-center sm:items-center sm:h-[80%] sm:text-center  bg-cover bg-center"
                 style={{backgroundImage: `url(${Bg})`}}>
-                <h2 className=" sm:text-[30px]  sm:pt-[180px]  sm:w-[270px]  sm:leading-[40px] text-white text-[100px] w-[935px] pt-[320px] ml-14 leading-[100px]">
+                <h2 className=" lg:text-[70px] lg:w-[80%] sm:text-[30px]  sm:pt-[180px]  sm:w-[270px]  sm:leading-[40px] text-white text-[100px] w-[935px] pt-[320px] ml-14 leading-[100px]">
                     Your Co-Working Journey starts here
                 </h2>
                 <button
@@ -22,13 +22,13 @@ function Index(props) {
             </div>
 
 
-            <div className="flex w-full h-auto pb-20 gap-80 pt-32 bg-[#EBEBEB] sm:flex-col">
+            <div className="flex w-full h-auto pb-20 gap-80 pt-32 bg-[#EBEBEB] sm:flex-col lg:flex-col">
                 <div className="ml-32 sm:ml-[30px]">
-                    <h3 className="text-[32px] text-[#2D2D2D]">Membership</h3>
+                    <h3 className="text-[32px] text-[#2D2D2D] lg:ml-[85px]">Membership</h3>
                 </div>
-                <div className={"sm:mt-[-280px] sm:ml-[30px]"}>
-                    <h2 className="text-[#8D8D8D] text-[32px]">120+ Places</h2>
-                    <p className="text-[20px] sm:w-[80%] text-[#2D2D2D] w-[390px] mt-8">
+                <div className={"sm:mt-[-280px] sm:ml-[30px] lg:ml-[210px]"}>
+                    <h2 className="text-[#8D8D8D] text-[32px] lg:mt-[-280px]">120+ Places</h2>
+                    <p className="text-[20px] sm:w-[80%] text-[#2D2D2D] w-[390px] mt-8 ">
                         Our co-working environment is more than just shared desks. It's a community-driven ecosystem
                         designed to elevate your work experience
                     </p>
@@ -36,9 +36,8 @@ function Index(props) {
             </div>
 
 
-            <div className={"flex w-full h-auto justify-center items-center gap-10 bg-[#EBEBEB] sm:flex-wrap "}>
-                <div
-                    className="w-[440px] sm:w-[90%] sm:h-auto h-[585px] bg-[#FFFFFF] border rounded-[20px] flex flex-col  p-8">
+            <div className={"flex w-full h-auto justify-center items-center gap-10 bg-[#EBEBEB] sm:flex-wrap lg:flex-wrap"}>
+                <div className="w-[440px] sm:w-[90%] sm:h-auto h-[585px] bg-[#FFFFFF] border rounded-[20px] flex flex-col  p-8">
                     <h3 className=" text-[#2D2D2D] mt-4 text-[28px]">Day Plan</h3>
                     <h2 className="text-[48px] text-[#2D2D2D] mt-1 sm:text-[28px]">70 000 uzs<span
                         className="text-[#797979] text-[20px] sm:text-[18px]">/Day </span></h2>
@@ -64,11 +63,10 @@ function Index(props) {
                         Booking space
                     </button>
                 < /div>
-                <div
-                    className="w-[440px] sm:w-[90%] sm:h-auto h-[585px] bg-[#D1D1D1] border rounded-[20px] flex flex-col  p-8">
+                <div className="w-[440px] sm:w-[90%] sm:h-auto h-[585px] bg-[#D1D1D1] border rounded-[20px] flex flex-col  p-8">
                     <h3 className=" text-[#2D2D2D] mt-4 text-[28px]">Flex Plan</h3>
-                    <h2 className="text-[48px] text-[#2D2D2D] mt-1 sm:text-[28px]">1,7 mln uzs <span
-                        className="text-[#797979] text-[20px] sm:text-[18px]">/Month</span></h2>
+                    <h2 className="text-[48px] text-[#2D2D2D] mt-1 sm:text-[28px]">1,7 mln uzs<span
+                        className="text-[#797979] text-[20px] sm:text-[18px]">/Month </span></h2>
                     <div className="mt-8 w-full flex flex-col gap-4">
                         <p className="flex items-center gap-2 text-[16px] text-[#797979]">
                             <img className="w-[16px] h-[17px]" src={Chek} alt=""/>
@@ -78,27 +76,24 @@ function Index(props) {
                             <img className="w-[16px] h-[17px]" src={Chek} alt=""/>
                             High-speed internet and tech amonites
                         </p>
-                        <p className="flex items-center gap-2 text-[16px] text-[#797979] w-[450px] sm:w-[95%]">
+                        <p className="flex items-center gap-2 text-[16px] text-[#797979] w-[450px] sm:w-[90%]">
                             <img className="w-[16px] h-[17px]" src={Chek} alt=""/>
-                            Community events and networking opportunites
-                        </p>
-                        <p className="flex items-center gap-2 text-[16px] text-[#797979] w-[450px]">
+                            Community events and networking opportunites</p>
+                        <p className="flex items-center gap-2 text-[16px] text-[#797979] w-[450px] sm:w-[90%]">
                             <img className="w-[16px] h-[17px]" src={Chek} alt=""/>
-                            Printer 15 pages
-                        </p>
+                            Printer 15 pages</p>
                         <p className="flex items-center gap-2 text-[16px] text-[#797979]">
                             <img className="w-[16px] h-[17px]" src={Chek1} alt=""/>
-                            Free water, tea, coffee
-                        </p>
+                            Free water, tea, coffee                        </p>
                     </div>
                     <button
-                        className=" sm:w-[90%] w-[347px] mt-[90px]  ml-3 h-[60px] bg-[#373737] rounded-[90px] text-white text-[19px]">
+                        className="   sm:w-[90%] w-[347px] mt-[130px]  ml-3 h-[60px] bg-[#373737] rounded-[90px] text-white text-[19px]  ">
                         Booking space
                     </button>
                 < /div>
 
-                <div
-                    className="w-[440px] sm:w-[90%] sm:h-auto h-[585px] bg-[#2D2D2D] border rounded-[20px] flex flex-col  p-8">
+
+                <div className="w-[440px] sm:w-[90%] sm:h-auto h-[585px] bg-[#2D2D2D] border rounded-[20px] flex flex-col  p-8">
                     <h3 className=" text-[#FFFFFF] mt-4 text-[28px]">Fix Plan</h3>
                     <h2 className="text-[48px] text-[#FFFFFF] mt-1 sm:text-[28px]">2,2 mln uzs <span
                         className="text-[#797979] text-[18px]">/Month</span></h2>
@@ -132,8 +127,7 @@ function Index(props) {
             </div>
 
 
-            <div className={"w-full h-screen flex justify-center items-center relative  bg-[#EBEBEB]"}
-                 style={{backgroundImage: ` url(${Grn})`}}>
+            <div className={"w-full h-screen flex justify-center items-center relative  bg-[#EBEBEB]"} style={{backgroundImage: ` url(${Grn})`}}>
                 <img className={"w-[90%] absolute z-10 sm:w-full sm:h-[600px] lg:h-[600px]"} src={Grn} alt=""/>
                 <div className="absolute z-20 ml-[-600px] sm:ml-0 lg:ml-[-40px]">
                     <button
