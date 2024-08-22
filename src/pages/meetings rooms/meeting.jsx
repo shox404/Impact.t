a9192b2812be551b54476d191efa44ab87944398
IbragimovaG <ibragimoffa.gul0706@gmail.com>
2024-08-22 10:42:15 +0500

    AnimationAll

--- src/pages/meetings rooms/meeting.jsx
+++ src/pages/meetings rooms/meeting.jsx
@@ -16,19 +16,19 @@ function Meeting(props) {
             <div
                 className="w-full h-screen bg-black   sm:justify-center sm:items-center sm:h-[80%] sm:text-center  bg-cover bg-center"
                 style={{backgroundImage: `url(${Bg})`}}>
-                <h2 className=" lg:text-[70px] lg:w-[80%] sm:text-[30px]  sm:pt-[180px]  sm:w-[270px]  sm:leading-[40px] text-white text-[100px] w-[955px] pt-[320px] ml-14 leading-[100px]">
+                <h2 data-aos='fade-right' data-aos-duration='1000' className=" lg:text-[70px] lg:w-[80%] sm:text-[30px]  sm:pt-[180px]  sm:w-[270px]  sm:leading-[40px] text-white text-[100px] w-[955px] pt-[320px] ml-14 leading-[100px]">
                     Unlock Our dynamic Meeting rooms
                 </h2>
-                <button
+                <button data-aos='fade-right' data-aos-duration='1000' data-aos-delay='200'
                     className={"w-[180px] mt-20 ml-[50px] sm:w-[160px] sm:text-[13px]  sm:h-[45px]  sm:ml-[-20px] sm:mt-[50px]    text-[15px] h-[50px] bg-white rounded-[90px] text-[#2D2D2D]  sm:mb-64"}>BOOKING
                     SPACE
                 </button>
             </div>
             <div className="flex w-full h-auto pb-20 gap-80 pt-32 bg-[#EBEBEB] sm:flex-col lg:flex-col ">
-                <div className="ml-32 sm:ml-[30px]">
+                <div data-aos='zoom-in' className="ml-32 sm:ml-[30px]">
                     <h3 className="text-[32px] text-[#2D2D2D] lg:ml-[85px] md:ml-[10px] md:mb-[280px]">Tariff plan</h3>
                 </div>
-                <div className={"sm:mt-[-280px] sm:ml-[30px] lg:ml-[210px]"}>
+                <div data-aos='fade-up' className={"sm:mt-[-280px] sm:ml-[30px] lg:ml-[210px]"}>
                     <h2 className="text-[#8D8D8D] text-[32px] lg:mt-[-280px]">5+ Meeting rooms</h2>
                     <p className="text-[20px] sm:w-[80%] text-[#2D2D2D] w-[390px] mt-8 ">
                         Our meeting rooms blend functionality with sophistication, providing the perfect setting for
@@ -38,12 +38,12 @@ function Meeting(props) {
 
             </div>
             <div className={"w-full flex-wrap gap-2  h-[150vh] flex sm:h-[460vh] lg:h-[170vh]"}>
-                <div className="w-[630px] h-[335px] bg-white ml-8 rounded-[20px] sm:w-full sm:flex sm:flex-col sm:h-[100vh] sm:ml-0 sm:mb-[-280px] lg:h-[335px] lg:mb-[-280px]">
+                <div  data-aos='zoom-in' data-aos-duration='1000' className="w-[630px] h-[335px] bg-white ml-8 rounded-[20px] sm:w-full sm:flex sm:flex-col sm:h-[100vh] sm:ml-0 sm:mb-[-280px] lg:h-[335px] lg:mb-[-280px]">
 
                     <img src={Conference} alt=""/>
 
 
-                    <div className={"ml-[330px] mt-[-310px]  sm:ml-0 sm:mt-0 sm:pl-4 sm:pt-2"}>
+                    <div data-aos='fade-up' data-aos-duration='1000' className={"ml-[330px] mt-[-310px]  sm:ml-0 sm:mt-0 sm:pl-4 sm:pt-2"}>
                         <h3>Conference Room “Orange”</h3>
                         <h2 className="text-[30px] text-[#2D2D2D] mt-1 sm:text-[30px]">1 00 000 uzs<span
                             className="text-[#797979] text-[20px] sm:text-[18px]">/Hourse </span></h2>
@@ -86,12 +86,12 @@ function Meeting(props) {
                         </button>
                     </div>
                 </div>
-                <div className="w-[630px] h-[335px] bg-white ml-6 rounded-[20px] sm:w-full sm:flex sm:flex-col sm:h-[100vh] sm:ml-0 lg:h-[335px]">
+                <div  data-aos='zoom-in' data-aos-duration='1000' className="w-[630px] h-[335px] bg-white ml-6 rounded-[20px] sm:w-full sm:flex sm:flex-col sm:h-[100vh] sm:ml-0 lg:h-[335px]">
 
                     <img src={Conference2} alt=""/>
 
 
-                    <div className={"ml-[330px] mt-[-310px] sm:ml-0 sm:mt-0 sm:pl-4 sm:pt-2 "}>
+                    <div data-aos='fade-up' data-aos-duration='1000' className={"ml-[330px] mt-[-310px] sm:ml-0 sm:mt-0 sm:pl-4 sm:pt-2 "}>
                         <h3>Conference Room “Blue”</h3>
                         <h2 className="text-[30px] text-[#2D2D2D] mt-1 sm:text-[30px]">1 00 000 uzs<span
                             className="text-[#797979] text-[20px] sm:text-[18px]">/Hourse </span></h2>
@@ -133,12 +133,12 @@ function Meeting(props) {
                         </button>
                     </div>
                 </div>
-                <div className="w-[630px] mt-[-240px] h-[335px]  bg-white ml-8 rounded-[20px] sm:w-full sm:flex sm:flex-col sm:h-[100vh] sm:ml-0 lg:h-[335px]">
+                <div data-aos='zoom-in' data-aos-duration='1000' className="w-[630px] mt-[-240px] h-[335px]  bg-white ml-8 rounded-[20px] sm:w-full sm:flex sm:flex-col sm:h-[100vh] sm:ml-0 lg:h-[335px]">
 
                     <img src={Conference3} alt=""/>
 
 
-                    <div className={"ml-[330px] mt-[-310px] sm:ml-0 sm:mt-0 sm:pl-4 sm:pt-2  "}>
+                    <div data-aos='fade-up' data-aos-duration='1000' className={"ml-[330px] mt-[-310px] sm:ml-0 sm:mt-0 sm:pl-4 sm:pt-2  "}>
                         <h3>Conference Room “Blue”</h3>
                         <h2 className="text-[30px] text-[#2D2D2D] mt-1 sm:text-[30px]">1 50 000 uzs<span
                             className="text-[#797979] text-[20px] sm:text-[18px]">/Hourse </span></h2>
@@ -180,12 +180,9 @@ function Meeting(props) {
                         </button>
                     </div>
                 </div>
-                <div className="w-[630px] mt-[-240px] h-[335px] bg-white ml-6 rounded-[20px] sm:w-full sm:flex sm:flex-col sm:h-[100vh] sm:ml-0 lg:h-[335px]">
-
+                <div data-aos='zoom-in' data-aos-duration='1000' className="w-[630px] mt-[-240px] h-[335px] bg-white ml-6 rounded-[20px] sm:w-full sm:flex sm:flex-col sm:h-[100vh] sm:ml-0 lg:h-[335px]">
                     <img src={Conference4} alt=""/>
-
-
-                    <div className={"ml-[330px] mt-[-310px]  sm:ml-0 sm:mt-0 sm:pl-4 sm:pt-2 "}>
+                    <div data-aos='fade-up' data-aos-duration='1000' className={"ml-[330px] mt-[-310px]  sm:ml-0 sm:mt-0 sm:pl-4 sm:pt-2 "}>
                         <h3>Conference Room “Blue”</h3>
                         <h2 className="text-[30px] text-[#2D2D2D] mt-1 sm:text-[30px]">1 80 000 uzs<span
                             className="text-[#797979] text-[20px] sm:text-[18px]">/Hourse </span></h2>
@@ -227,13 +224,10 @@ function Meeting(props) {
                         </button>
                     </div>
                 </div>
-
-
             </div>
-
-            <div className={"w-full h-screen flex justify-center items-center relative mt-[-250px] bg-[#EBEBEB]"}
+            <div  className={"w-full h-screen flex justify-center items-center relative mt-[-250px] bg-[#EBEBEB]"}
                  style={{backgroundImage: ` url(${Grn})`}}>
-                <img className={"w-[90%] absolute z-10 sm:w-full sm:h-[600px] lg:h-[600px]"} src={Grn} alt=""/>
+                <img data-aos='fade-up git' className={"w-[90%] absolute z-10 sm:w-full sm:h-[600px] lg:h-[600px]"} src={Grn} alt=""/>
                 <div className="absolute z-20 ml-[-600px] sm:ml-0 lg:ml-[-40px]">
                     <button
                         className={"w-[180px] mb-72  ml-10  text-[15px] h-[50px] sm:ml-5 sm:mb-10 lg:ml-[-40px] bg-white rounded-[90px] text-[#2D2D2D]"}>GALLERY
